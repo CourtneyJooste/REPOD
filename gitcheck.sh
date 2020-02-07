86bf9b9c082ea1eb21b025261102f4b69dd48f76
Courtney Jooste <courtney@codelab.io>
2020-02-07 15:19:53 +0200

    feat: updated and improved readme. added fixes to repod

--- gitcheck.sh
+++ gitcheck.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 prefix="[REPOD GITCHECK]"
 
 function prepend() { while read line; do echo "${prefix} ${line}"; done; }
@@ -9,6 +11,9 @@ else
     if [ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | sed 's/\// /g') | cut -f1) ]; then
         echo Repository is up-to-date | prepend
     else
+        git stash
+        git stash drop
+        git pull
         docker build -t $1 .
         docker tag $1 localhost:5000/$1 | prepend
         docker push localhost:5000/$1 | prepend
