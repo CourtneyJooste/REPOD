03e43343b2a0e4d8c2f76d42e9641ecf9cba03d8
Courtney Jooste <courtney@codelab.io>
2020-12-08 21:17:37 +0200

    fix: improvements to refresh rate

--- repod.js
+++ repod.js
@@ -1,6 +1,6 @@
 const shell = require('shelljs');
 
-const refresh = process.env.REFRESH_RATE;
+const refresh = process.env.REFRESH_RATE || 10;
 const tag = process.env.TAG;
 const webhook = process.env.SLACK_WEBHOOK;
 
@@ -14,13 +14,15 @@ if (refresh <= 0) {
     return;
 }
 
+const ONE_MINUTE = 60 * 1000;
+
 const runScript = () => {
     console.log("[REPOD] Executing gitcheck...");
     shell.exec('./gitcheck.sh ' + tag + (webhook ? ' ' + webhook : ''));
     const now = new Date();
-    const nextRun = now.setMinutes(now.getMinutes() + refresh);
+    const nextRun = new Date(currentDate.getTime() + refresh * ONE_MINUTE);
     console.log("[REPOD] Gitcheck process complete. Executing next run at " + (new Date(nextRun)).toTimeString());
-    setTimeout(() => runScript(), refresh * 60 * 1000);
+    setTimeout(() => runScript(), refresh * ONE_MINUTE);
 }
 
 console.log("[REPOD] Running repository update daemon...");
